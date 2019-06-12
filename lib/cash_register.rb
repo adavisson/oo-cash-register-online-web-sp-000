4722cbb098edc461543afc259f88d4e832205cc3
Andrew Davisson <akdavisson4@gmail.com>
2019-06-12 22:18:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,11 +14,7 @@ class CashRegister
   end
   
   def apply_discount
-    if @discount > 0
-      @total * ((100.0 - @discount) / 100.0)
-    else
-      @total
-    end
+    @total.to_f * ((100.0 - @discount.to_f) / 100.0)
   end
   
 end
